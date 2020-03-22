b6d9a12d5e0609ed9c954281863040c339ebbe91
Alex Gallardo <gallardoalex069@gmail.com>
2020-03-21 23:02:48 -0600

    update changes app

--- src/actions/app.js
+++ src/actions/app.js
@@ -1,22 +1,12 @@
-/**
-@license
-Copyright (c) 2018 The Polymer Project Authors. All rights reserved.
-This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
-The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
-The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
-Code distributed by Google as part of the polymer project is also
-subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
-*/
-
 export const UPDATE_PAGE = 'UPDATE_PAGE';
 export const UPDATE_OFFLINE = 'UPDATE_OFFLINE';
 export const UPDATE_DRAWER_STATE = 'UPDATE_DRAWER_STATE';
 export const OPEN_SNACKBAR = 'OPEN_SNACKBAR';
 export const CLOSE_SNACKBAR = 'CLOSE_SNACKBAR';
 
+// Funciones del path
 export const navigate = (path) => (dispatch) => {
-  // Extract the page name from path.
-  const page = path === '/' ? 'view1' : path.slice(1);
+  const page = path === '/' ? 'inicio' : path.slice(1);
 
   // Any other info you might want to extract from the path (like page type),
   // you can do here
@@ -28,16 +18,22 @@ export const navigate = (path) => (dispatch) => {
 
 const loadPage = (page) => (dispatch) => {
   switch(page) {
-    case 'view1':
+    case 'inicio':
       import('../components/my-view1.js').then((module) => {
         // Put code in here that you want to run every time when
         // navigating to view1 after my-view1.js is loaded.
       });
       break;
-    case 'view2':
+    case 'home':
       import('../components/my-view2.js');
       break;
-    case 'view3':
+    case 'data':
+      import('../components/my-view3.js');
+      break;
+    case 'environment':
+      import('../components/my-view3.js');
+      break;
+    case 'location':
       import('../components/my-view3.js');
       break;
     default:
