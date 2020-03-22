e5b5525a17dda2d06336bb3d26e976ec9f714139
Alex Gallardo <gallardoalex069@gmail.com>
2020-03-21 21:41:12 -0600

    add proyect

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+
+// console.info('Service worker disabled for development, will be generated at build time.');
+
+self.addEventListener('fetch', event => {
+
+    console.log(event)
+
+    if (event.request.url.includes('.css')) {
+        event.respondWith(null)
+    } else {
+        event.respondWith(fetch(event.request));
+    }
+});
